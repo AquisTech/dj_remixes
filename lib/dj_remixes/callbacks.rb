3d3106bf307ce6594f201769fb3295b2eb118e79
AquisTech <anand.bait@gmail.com>
2018-07-19 12:21:38 +0530

    Instead of using touch used conditional assignment

--- lib/dj_remixes/callbacks.rb
+++ lib/dj_remixes/callbacks.rb
@@ -1,22 +1,22 @@
 module DJ
   class Worker
-    
+
     def before(job)
       self.dj_object = job
-      job.touch(:started_at)
+      job.persisted? ? job.touch(:started_at) : job.started_at = Time.now.utc
     end
-    
+
     def after(job)
     end
-    
+
     def success(job)
-      job.touch(:finished_at)
+      job.persisted? ? job.touch(:finished_at) : job.finished_at = Time.now.utc
       self.enqueue_again if self.respond_to?(:enqueue_again)
     end
-    
+
     def error(job, error)
       job.update_attributes(:started_at => nil)
     end
-    
+
   end # Worker
 end # DJ
